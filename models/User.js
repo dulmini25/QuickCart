f5758855cec0c929b958aaee5f8c036f8ccd9653
Dul_miii <dulminidilhara2001@gmail.com>
2025-12-18 23:25:45 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,13 @@
+
+import { User } from "@clerk/nextjs/dist/types/server"
+import mongoose from mongoose
+const userSchema = new mongoose.Schema({
+    _id : {type: String, required:true},
+    name : {type: String, required:true},
+    email : {type: String, required:true, unique:true},
+    imageUrl : {type: String, required:true},
+    cardItems : {type:Object, default:{}}
+} , {minimize: false})
+
+const user = mongoose.models.user || mongoose.model('user',userSchema)
+export default User 
